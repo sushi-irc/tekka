7e8b130ad10563b088e244ac5db7c52907ebee1e
Marian Tietz <pronox@gmail.com>
2008-11-05 01:15:57 +0100

    Added apply-methods for preferences dialog

--- dialogs/preferencesDialog.py
+++ dialogs/preferencesDialog.py
@@ -31,9 +31,9 @@ def fillChild(category, table, child):
 		value = int (config.get(category, name, ""))
 		child.set_value(value)
 
-def fillValues():
+def fillValues(categories):
 	
-	for category in ("tekka","colors","chatting"):
+	for category in categories:
 
 		table = widgets.get_widget("%sTable" % category)
 
@@ -41,11 +41,54 @@ def fillValues():
 			print "table '%sTable' not found." % category
 
 		for child in table.get_children():
+
+			# This i more like a hack for subcontainer (*Box)
+			# to handle their childs, too
 			if type(child) == gtk.HBox or type(child) == gtk.VBox:
 				for subchild in child.get_children():
 					fillChild(category, child, subchild)
+			
 			fillChild(category, table, child)
 
+def applyChild(category, table, child):
+	name = child.get_property("name")
+
+	if type(child) == gtk.CheckButton:
+		value = str(child.get_active()).lower()
+		config.set(category, name, value)
+		
+	elif type(child) == gtk.Entry:
+		value = child.get_text()
+		config.set(category, name, value)
+
+	elif type(child) == gtk.SpinButton:
+		value = str(int(child.get_value()))
+		config.set(category, name, value)
+
+def applyValues(categories):
+	
+	for category in categories:
+
+		table = widgets.get_widget("%sTable" % category)
+
+		if not table:
+			print "table '%sTable' not found." % category
+
+		for child in table.get_children():
+
+			# This i more like a hack for subcontainer (*Box)
+			# to handle their childs, too
+			if type(child) == gtk.HBox or type(child) == gtk.VBox:
+				for subchild in child.get_children():
+					applyChild(category, child, subchild)
+			
+			applyChild(category, table, child)
+
+def tekka_output_font_fontSelectionButton_clicked_cb(button):
+	pass
+
+def tekka_general_output_font_fontSelectionButton_clicked_cb(button):
+	pass
 
 def setup(dialogInterface):
 	"""
@@ -55,12 +98,24 @@ def setup(dialogInterface):
 
 	widgets = gtk.glade.XML(config.get("gladefiles","dialogs"), "preferencesDialog")
 
+	sigdic = {
+		"tekka_output_font_fontSelectionButton_clicked_cb" :
+			tekka_output_font_fontSelectionButton_clicked_cb,
+		"tekka_general_output_font_fontSelectionButton_clicked_cb" :
+			tekka_general_output_font_fontSelectionButton_clicked_cb,
+	}
+
+	widgets.signal_autoconnect(sigdic)
+
 def run():
+	categories = ("tekka","colors","chatting")
 	dialog = widgets.get_widget("preferencesDialog")
 	
-	fillValues()
+	fillValues(categories)
 
-	dialog.run()
+	if dialog.run() == gtk.RESPONSE_OK:
+		applyValues(categories)
+		# TODO: restart everything
 
 	dialog.destroy()
 
