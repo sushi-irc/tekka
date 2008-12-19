54a1c4161b6d0e94c1d557e66bba84c0b45c5049
Marian Tietz <nemo@ikkoku.de>
2008-12-19 22:49:55 +0100

    Simplified channel list dialog

--- dialogs/channelList.py
+++ dialogs/channelList.py
@@ -26,7 +26,7 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 SUCH DAMAGE.
 """
 
-# TODO:  add a status bar or something which 
+# TODO:  add a status bar or something which
 # TODO:: reports that the listing is done
 
 from re import compile
@@ -44,6 +44,8 @@ widgets = None
 currentServer = None
 filterExpression = None
 listView = None
+handler = None
+check = None
 cache = []  # /list cache
 
 def run(server):
@@ -63,24 +65,18 @@ def run(server):
 
 	dialog = widgets.get_widget("channelList")
 
-	result = dialog.run()
-
-	while result not in (gtk.RESPONSE_CANCEL,
-						gtk.RESPONSE_DELETE_EVENT):
-		result = dialog.run()
+	dialog.run()
 
 	dialog.destroy()
 
 	return True # o.0
 
-def resetSignal(*x):
-	print "reseting signal!"
-	signals.list.func_code = signals.list.func_globals["myCode"]
-	del signals.list.func_globals["me"]
-	del signals.list.func_globals["myCode"]
-	del signals.list.func_globals["listView"]
-	del signals.list.func_globals["filterExpression"]
-	del signals.list.func_globals["cache"]
+def resetSignal():
+	""" reset the list signal to the original state """
+	global check
+	if not check:
+		check = com.sushi.connect_to_signal("list", signals.list)
+	handler.remove()
 
 def listButton_clicked_cb(button):
 	"""
@@ -106,45 +102,22 @@ def listButton_clicked_cb(button):
 		# use cached values
 		for (server, channel, user, topic) in cache:
 			sushiList(0, server, channel, user, topic)
+	else:
+		# redirect "list" signal
+		com.bus.remove_signal_receiver(
+				signals.list,
+				"list",
+				com.sushi.dbus_interface,
+				com.sushi.bus_name,
+				com.sushi.object_path)
+		global handler
+		handler = com.sushi.connect_to_signal("list", sushiList)
 
-		return
-
-	"""
-		ok the next lines need some explanation.
-		we have the signals module which catches the list
-		signal. but this time we don't want this because
-		this dialog should catch the listing.
-		So we're replacing the func_code object (compiled
-		function code) from the list function of signals
-		and replace it with the func_code of our
-		list-function.
-		At list end we set this back to the old code.
-
-		Because the func_code has at the execution time
-		another environment we have to set the used
-		global vars into func_globals dict of the function.
-
-		FIXME:  may be a better solution would be great.
-		FIXME:: if something is going wrong this could
-		FIXME:: fuck up the whole usability (list would
-		FIXME:: not workd and may be python mess arround
-		FIXME:: due to wasted functions.
-	"""
-	signals.list.func_globals["me"] = signals.list
-	signals.list.func_globals["myCode"] = signals.list.func_code
-	signals.list.func_globals["listView"] = listView
-	signals.list.func_globals["filterExpression"] = filterExpression
-	signals.list.func_globals["cache"] = cache
-
-	signals.list.func_code = sushiList.func_code
-
-	widgets.get_widget("channelList").connect("close", resetSignal)
-	widgets.get_widget("channelList").connect("destroy", resetSignal)
-
-	try:
-		com.list(currentServer)
-	except:
-		resetSignal()
+		try:
+			com.list(currentServer)
+		except BaseException, e:
+			print e
+			resetSignal()
 
 def listView_row_activated_cb(treeView, path, column):
 	"""
@@ -167,7 +140,6 @@ def sushiList(time, server, channel, user, topic):
 	receives the data from maki.
 	add server/user/topic to listStore
 	"""
-
 	if time > 0:
 		# no manual call
 		cache.append((server,channel,user,topic))
@@ -176,16 +148,7 @@ def sushiList(time, server, channel, user, topic):
 		# EOL, this is not reached if we use
 		# manual call.
 
-		try:
-			me.func_code = myCode
-		except NameError:
-			return
-
-		del me.func_globals["cache"]
-		del me.func_globals["listView"]
-		del me.func_globals["filterExpression"]
-		del me.func_globals["myCode"]
-		del me.func_globals["me"]
+		resetSignal()
 
 		return
 
