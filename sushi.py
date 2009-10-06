8208376343b10468a26d85a242e2c4f31d39b3cb
Marian Tietz <nemo@ikkoku.de>
2009-10-07 01:12:40 +0200

    Advanced sushi plugin options

--- sushi.py
+++ sushi.py
@@ -39,7 +39,7 @@ TYPE_STRING: Takes one argument (default string)
 TYPE_PASSWORD: Hidden string. Takes one argument (default string)
 TYPE_NUMBER: Takes one argument (default number)
 TYPE_BOOL: Takes one argument (default bool value)
-TYPE_CHOICE: Takes n key/value tuple.
+TYPE_CHOICE: Takes n key/value tuple and a default index.
 """
 (TYPE_STRING,
  TYPE_PASSWORD,
