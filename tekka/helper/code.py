9134b95fdd421cba11ba7f89c409290278f4941d
Marian Tietz <nemo@ikkoku.de>
2009-12-26 02:29:57 +0100

    Introduce reset_function_attrs

--- tekka/helper/code.py
+++ tekka/helper/code.py
@@ -1,6 +1,9 @@
 
 """ Misc. code helper """
 
+def _generate_unique_attribute(fun):
+	return "__init_%s_%s" % (fun.func_name, str(id(fun)))
+
 def init_function_attrs(fun, **vars):
 	""" Add the variables with values as attributes to the function fun
 		if they do not exist and return the function.
@@ -8,9 +11,18 @@ def init_function_attrs(fun, **vars):
 		Usage: self = init_function_attr(myFun, a = 2, b = 3)
 		Results in: self = myFun, self.a = 2, self.b = 3
 	"""
-	for (key, val) in vars.items():
-		try:
-			getattr(fun, key)
-		except AttributeError:
+	try:
+		getattr(fun, _generate_unique_attribute(fun))
+	except AttributeError:
+		for (key, val) in vars.items():
 			setattr(fun, key, val)
+
 	return fun
+
+def reset_function_attrs(fun):
+	try:
+		getattr(fun, _generate_unique_attribute(fun))
+	except AttributeError:
+		pass
+	else:
+		delattr(fun, _generate_unique_attribute(fun))
