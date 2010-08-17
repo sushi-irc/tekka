2065a8c209a3a367c8152dd8b3a2f347155090fe
Marian Tietz <nemo@ikkoku.de>
2010-08-17 03:02:28 +0200

    Fix missing import

--- sushi.py
+++ sushi.py
@@ -26,6 +26,8 @@ OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
 SUCH DAMAGE.
 """
 
+from gettext import gettext as _
+
 from tekka import com
 from tekka import config
 from tekka import commands
